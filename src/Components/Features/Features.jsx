eba1e9d0795d5bed98cfedb4a64a35e49ad98e3d
Shermin Sultana <rima64549@gmail.com>
2023-09-18 04:06:59 +0600

    add button and make hover

--- src/Components/Features/Features.jsx
+++ src/Components/Features/Features.jsx
@@ -1,12 +1,14 @@
 
 import PropTypes from 'prop-types'
+import { AiFillCheckCircle } from 'react-icons/ai';
 
 const Features = ({feature})=> {
     
   return (
     <div>
-        <p>{feature}</p>
+        <p className='mt-4 flex items-center gap-2'> <AiFillCheckCircle className='text-green-600'></AiFillCheckCircle>{feature}</p>
     </div>
+    
   )
 }
 
--- src/Components/Priceoption/Priceoption.jsx
+++ src/Components/Priceoption/Priceoption.jsx
@@ -7,14 +7,17 @@ import Features from '../Features/Features';
 const Priceoption = ({option}) => {
     const {name, price, features} = option ;
   return (
-    <div className='bg-blue-500 p-7 text-center text-white font-bold rounded'>
-       <h2 className='text-3xl mb-3'>
+    <div className='bg-blue-500 p-7  text-white rounded flex flex-col'>
+       <h2 className='text-3xl  font-bold  mb-3 text-center'>
         <span>{price}</span>
        </h2>
-       <h3 className='text-2xl'>{name}</h3>
-       {
+       <h3 className='text-2xl text-center'>{name}</h3>
+      <div className='flex-grow'>
+      {
        features.map((feature,idx) => <Features key={idx} feature={feature}></Features>)
     }
+      </div>
+      <button className=' px-7 p-3  bg-white text-blue-600 rounded mt-8  hover:bg-green-400'>Buy now</button> 
     </div>
     
   )
